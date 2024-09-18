0e6d442ebab3e74d9769906fcd9061b046723146
YuYoHan <zxzz8014@naver.com>
2024-09-18 20:26:38 +0900

    [Add] yml 설정 추가

---
+++ src/main/java/com/example/meettify/MeettifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.meettify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeettifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeettifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meettify/MeettifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meettify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeettifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
