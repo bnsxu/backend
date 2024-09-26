4fb2e8c43a457f575f48a80ab093d2ced1a0a326
YuYoHan <zxzz8014@naver.com>
2024-09-26 09:35:15 +0900

    [Modify] JWT 검증 로직 수정

--- src/main/java/com/example/meettify/MeettifyApplication.java
+++ src/main/java/com/example/meettify/MeettifyApplication.java
@@ -3,12 +3,7 @@ package com.example.meettify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-        exclude = {
-                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-                org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-                org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-        })
+@SpringBootApplication()
 public class MeettifyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/meettify/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/meettify/config/jwt/JwtAuthenticationFilter.java
@@ -17,32 +17,50 @@ import java.nio.file.AccessDeniedException;
 
 @Log4j2
 @RequiredArgsConstructor
+// OncePerRequestFilter을 하는 이유는 한번만 작동하도록 하기 위해서 입니다.
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private static final String AUTH_HEADER = "Authorization";
+    public static final String HEADER_AUTHORIZATION = "Authorization";
     private final JwtProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-        HttpServletRequest httpServletRequest = request;
-        String token = resolveToken(httpServletRequest);
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
 
-        if(token != null && jwtProvider.validateToken(token)) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+
+        // request에서 JWT를 추출
+        // 요청 헤더에서 토큰을 추출하는 역할
+        String token = resolveToken(httpServletRequest);
+        log.info("token : " + token);
+
+        // 토큰이 존재하고 토큰 유효성 검사를 통과하면 true
+        if(StringUtils.hasText(token) && jwtProvider.validateToken(token)) {
+            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext에 저장
+            // 여기까지 통과하면 토큰은 인증도 받았고 권한도 있다.
             Authentication authentication = jwtProvider.getAuthentication(token);
+            // Spring Security의 SecurityContextHolder를 사용하여 현재 인증 정보를 설정합니다.
+            // 이를 통해 현재 사용자가 인증된 상태로 처리됩니다.
+            // 이렇게 저장하면 컨트롤러에서 토큰에서 정보를 가져와서 사용할 수 있습니다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } else {
-            throw new AccessDeniedException("접근 권한이 없습니다.");
+            log.error("유효한 토큰이 없습니다.");
         }
         filterChain.doFilter(request, response);
     }
 
-    private String resolveToken(HttpServletRequest request) {
-        String token = request.getHeader(AUTH_HEADER);
-        log.info("token: {}", token);
+    // 토큰을 가져오기 위한 메소드
+    // Authorization로 정의된 헤더 이름을 사용하여 토큰을 찾고
+    // 토큰이 "Bearer "로 시작하거나 "Bearer "로 안온 것도 토큰 반환
+    private String resolveToken(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader(HEADER_AUTHORIZATION);
 
-        if(token.startsWith("Bearer ") && StringUtils.hasText(token)) {
+        // 토큰이 포함하거나 Bearer 로 시작하면 true
+        if(StringUtils.hasText(token) && token.startsWith("Bearer ")) {
             return token.substring(7);
+        } else if(StringUtils.hasText(token)) {
+            return token;
         } else {
             return null;
         }
