8fac3059d1d592638bfc85f4a9810ab8b1649474
YuYoHan <zxzz8014@naver.com>
2024-09-28 13:05:21 +0900

    [Modify] swagger 설정 수정

--- src/main/java/com/example/meettify/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/meettify/config/swagger/SwaggerConfig.java
@@ -1,36 +1,17 @@
 package com.example.meettify.config.swagger;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
-    private static final String REFERENCE = "Bearer";
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)// OpenAPI 3.0
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.backend.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .useDefaultResponseMessages(false);
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Meettify API 문서")
-                .description("주고받을 데이터를 문서화")
-                .version("1.0")
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Meettify API")
+                .pathsToMatch("/api/**")
                 .build();
     }
 }
